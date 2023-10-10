14c3d4f13d3fa2297e198ad356e308565c16e710
Caleb B <calebabaca@gmail.com>
2023-10-10 13:33:52 -0700

    finished code overview

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
- const routes = require('controllers');
+ const routes = require('./controllers');
  const helpers = require('./utils/helpers')
 
- const sequelize = require('./config/connection');
+ const sequelize = require('./config/connections');
  const sequelizeStore = require('connect-session-sequelize')(session.Store);
 
  const app = express();
@@ -21,7 +21,7 @@ const exphbs = require('express-handlebars');
     cookie: {},
     resave: false,
     saveUninitialized: true,
-    stpre: new SequalizeStore({
+    store: new SequalizeStore({
         db: sequelize
     })
  };
@@ -31,11 +31,11 @@ const exphbs = require('express-handlebars');
  //id express pn whicxh template
 
  app.engine('handlebars', hbs.engine);
- app.use('view engine', 'handlebars');
+ app.set('view engine', 'handlebars');
 
  app.use(express.json());
  app/use(express.urlencoded({ extended: true }));
- app.use(express.static(path.json(__dirname, 'public')));
+ app.use(express.static(path.join(__dirname, 'public')));
 
  app.use(routes);
 
